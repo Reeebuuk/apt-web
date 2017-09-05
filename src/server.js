ad42fe173b7b00deac1eee1448ff0845bc313c00
Krunoslav Uzelac <reeebuuk@gmail.com>
2017-09-05 10:45:52 +0400

    Fixing server.js

--- src/server.js
+++ src/server.js
@@ -1,5 +1,17 @@
 var connect = require('connect');
 var serveStatic = require('serve-static');
-connect().use(serveStatic(__dirname)).listen(9000, function(){
+var fs = require('fs');
+
+connect()
+    .use(serveStatic(__dirname))
+    .use((req, res, next) => {
+    fs.readFile(`${__dirname}/index.html`, (err, data) => {
+       if(err) throw err;
+
+       res.setHeader('Content-Length', data.length);
+       res.end(data);
+    });
+})
+.listen(9000, function () {
     console.log('Server running on 9000...');
 });
